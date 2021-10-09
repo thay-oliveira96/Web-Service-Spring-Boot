f25f21c27fe19389dfb6c21bc79d2d9008e9eac7
Thay-Oliveira96 <thaynan-rodrigues@outlook.com>
2021-10-09 19:40:22 -0300

    Projeto Criado

---
+++ src/main/java/com/trosoftware/Webservicejava/WebServiceJavaApplication.java
@@ -0,0 +1,13 @@
+package com.trosoftware.Webservicejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trosoftware/Webservicejava/WebServiceJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trosoftware.Webservicejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
