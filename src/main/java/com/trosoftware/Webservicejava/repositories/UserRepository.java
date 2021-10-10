b5ec762273a8298270c3438c3b23454e13166907
Thay-Oliveira96 <thaynan-rodrigues@outlook.com>
2021-10-09 22:39:07 -0300

    Implementacao de Interface Java

---
+++ src/main/java/com/trosoftware/Webservicejava/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.trosoftware.Webservicejava.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.trosoftware.Webservicejava.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	 
+}
+
